835109fffa2c75972cfb811d322aa1a504ead617
Tomasz Ducin <tomasz.ducin@gmail.com>
2016-03-15 22:04:47 +0100

    separated grunt task directory

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,46 +1,11 @@
 module.exports = function(grunt) {
 
     grunt.initConfig({
-        pkg: grunt.file.readJSON('package.json'),
-        connect: {
-            options: {
-                port: 9000,
-                hostname: 'localhost',
-                base: [
-                    'src',
-                    'bower_components'
-                ]
-            },
-            server: {
-                options: {
-                    open: true,
-                    livereload: 8999
-                }
-            }
-        },
-        watch: {
-            options: {
-                livereload: 8999
-                // processes are spawned by default (unspawned grunt fails often)
-            },
-            livereload: {
-                files: [
-                    'src/**/*'
-                ]
-            },
-            'index': {
-                files: ['src/index.html'],
-                tasks: [] // nothing to do here
-            }
-        }
+        pkg: grunt.file.readJSON('package.json')
     });
 
     require('load-grunt-tasks')(grunt);
 
-    grunt.registerTask('build', [
-        'connect',
-        'watch'
-    ]);
-
-    grunt.registerTask('default', ['build']);
+    grunt.registerTask('default', ['serve']);
+    grunt.task.loadTasks('tasks');
 };
---
+++ tasks/serve.js
@@ -0,0 +1,47 @@
+'use strict';
+
+module.exports = function (grunt) {
+
+    var serveConfig = {
+        connect: {
+            options: {
+                port: 9000,
+                hostname: 'localhost',
+                base: [
+                    'src',
+                    'bower_components'
+                ]
+            },
+            server: {
+                options: {
+                    open: true,
+                    livereload: 8999
+                }
+            }
+        },
+        watch: {
+            options: {
+                livereload: 8999
+                // processes are spawned by default (unspawned grunt fails often)
+            },
+            livereload: {
+                files: [
+                    'src/**/*'
+                ]
+            },
+            'index': {
+                files: ['src/index.html'],
+                tasks: [] // nothing to do here
+            }
+        }
+    };
+
+    grunt.config.merge(serveConfig);
+
+    var serveTasks = [
+        'connect',
+        'watch'
+    ];
+
+    grunt.registerTask('serve', 'serves application in development mode', serveTasks);
+};
